ce2f9dfe54ac2763dd724ea4d1e584d5d8cfc3d7
dntlkinslt <90123076+dntlkinslt@users.noreply.github.com>
2022-07-09 18:55:21 -0300

    Agregar archivos de proyecto.

---
+++ Settings Of Overlay.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace vamo_a_intentar_mandar_midi_a_traves_de_una_tecla_asheiii
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stateOfSus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.keyBinded = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // stateOfSus
+            // 
+            this.stateOfSus.Location = new System.Drawing.Point(2, 135);
+            this.stateOfSus.Name = "stateOfSus";
+            this.stateOfSus.Size = new System.Drawing.Size(271, 20);
+            this.stateOfSus.TabIndex = 0;
+            this.stateOfSus.Text = "Estado: Desactivado";
+            this.stateOfSus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(27, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Toca para configurar una tecla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // keyBinded
+            // 
+            this.keyBinded.Location = new System.Drawing.Point(2, 81);
+            this.keyBinded.Name = "keyBinded";
+            this.keyBinded.Size = new System.Drawing.Size(271, 25);
+            this.keyBinded.TabIndex = 3;
+            this.keyBinded.Text = "Tecla configurada: ";
+            this.keyBinded.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 192);
+            this.Controls.Add(this.keyBinded);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.stateOfSus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label stateOfSus;
+        private Button button1;
+        private Label keyBinded;
+    }
+}
---
+++ Settings Of Overlay.cs
@@ -0,0 +1,110 @@
+using System.Diagnostics;
+
+namespace vamo_a_intentar_mandar_midi_a_traves_de_una_tecla_asheiii
+{
+    public partial class Form1 : Form
+    {
+        NotifyIcon n = new NotifyIcon();
+        int sus = -1;
+        bool binding = false;
+        string key;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int w = Screen.PrimaryScreen.WorkingArea.Width, h = Screen.PrimaryScreen.WorkingArea.Size.Height;
+            Location = new Point(w - w / 6, h / 25);
+            key = Settings1.Default.Key;
+            RefreshKeyLabel();
+        }
+
+        void ToggleSus(int a)
+        {
+            if (a == 1) stateOfSus.Text = "Estado: Activado";
+            else if (a == -1) stateOfSus.Text = "Estado: Desactivado";
+            ExecuteCommand("sendmidi dev Sustain on 1 1", Directory.GetCurrentDirectory() + @"\");
+            sus *= -1;
+        }
+
+        void ExecuteCommand(string command, string dir)
+        {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", $"/c" + command);
+            processInfo.WorkingDirectory = dir;
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            // Warning: This approach can lead to deadlocks, see Edit #2
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+            process.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            binding = true;
+        }
+
+        void RefreshKeyLabel()
+        {
+            switch (key)
+            {
+                case "": keyBinded.Text = "No hay ninguna tecla configurada"; break;
+                case "Capital": keyBinded.Text = "Tecla configurada: Bloq Mayús"; break;
+                default: keyBinded.Text = $"Tecla configurada: {key}"; break;
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (binding)
+            {
+                key = e.KeyCode.ToString();
+                binding = false;
+                RefreshKeyLabel();
+                Settings1.Default.Key = key;
+                Settings1.Default.Save();
+            }
+            else if (e.KeyCode.ToString() == key)
+            {
+                ToggleSus(sus * -1);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+
+                n.Text = "Sustain Key";
+                n.Icon = Icon.ExtractAssociatedIcon("clave-de-sol.ico");
+                n.Click += N_Click;
+                n.Visible = true;
+                Hide();
+            }
+        }
+
+        private void N_Click(object? sender, EventArgs e)
+        {
+            n.Visible = false;
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
